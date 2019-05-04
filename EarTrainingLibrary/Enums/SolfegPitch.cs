04788911fe9f1c5ee1f1b945c5b64686d8cfefd2
Mike Hildner <mike.hildner@gmail.com>
2019-05-04 12:26:36 -0400

    Remove bold tags.

--- EarTrainingLibrary/Enums/SolfegPitch.cs
+++ EarTrainingLibrary/Enums/SolfegPitch.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EarTrainingLibrary.Enums
+﻿namespace EarTrainingLibrary.Enums
 {
     public enum SolfegPitch
     {
         Do = 0,         // Tonic
-        Ra = 1,         // Lowered 2
+        DiRa = 1,       // Lowered 2
         Re = 2,         // 2nd
-        Ma = 3,         // Minor 3rd
+        RiMe = 3,       // Minor 3rd
         Mi = 4,         // Major 3rd
         Fa = 5,         // Perfect 4th
-        Se = 6,         // Diminished 5th
+        FiSe = 6,       // Diminished 5th
         So = 7,         // Perfect 5th
-        Le = 8,         // Minor 6th
+        SiLe = 8,       // Minor 6th
         La = 9,         // Major 6th
-        Te = 10,        // Minor 7th
+        LiTe = 10,      // Minor 7th
         Ti = 11,        // Major 7th
         HighDo = 12
     }
