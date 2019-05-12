ea3819c820f6a43d6d9ad9d8b3c1d90beadafe94
Mike Hildner <mike.hildner@gmail.com>
2019-05-12 17:55:51 -0400

    Make all pitches page.

---
+++ EarTraining/Controllers/SolfegController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WaveLibrary;
+
+namespace EarTraining.Controllers
+{
+    public class SolfegController : BaseController
+    {
+        // GET: Solfeg
+        public ActionResult Index()
+        {
+            Pitch pitch = new Pitches().Random();
+            ViewBag.Pitch = pitch;
+
+            return View();
+        }
+    }
+}
