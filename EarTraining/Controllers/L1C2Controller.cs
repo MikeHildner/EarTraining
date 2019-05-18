0d4bc052e3533f5129028e3abe18b50ce8f4dd57
mhildner <mike.hildner@sunhydraulics.com>
2019-05-18 18:53:27 -0400

    Begin L1C2.

---
+++ EarTraining/Controllers/L1C2Controller.cs
@@ -0,0 +1,127 @@
+﻿using EarTrainingLibrary.Enums;
+using EarTrainingLibrary.NAudio;
+using EarTrainingLibrary.Utility;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WaveLibrary;
+
+namespace EarTraining.Controllers
+{
+    public class L1C2Controller : Controller
+    {
+        // GET: L1C2
+        public ActionResult Index()
+        {
+            Pitch pitch = new Pitches().Random();
+            ViewBag.Pitch = pitch;
+
+            return View();
+        }
+
+        public ActionResult GetDrill(double frequency, int type)
+        {
+            L1C2DrillType drillType = (L1C2DrillType)type;
+            var retMs = new MemoryStream();
+            MemoryStream wavStream = GetDrill(frequency, drillType);
+
+            wavStream.WavToMp3File(out string fileName);
+            return Redirect($"~/Temp/{fileName}");
+        }
+
+        private MemoryStream GetDrill(double frequency, L1C2DrillType drillType)
+        {
+            double bpm = 100;
+            double quarterNotemillis = (bpm / 60) * 1000;
+            TimeSpan noteDuration = TimeSpan.FromMilliseconds(quarterNotemillis);
+            double gain = 0.2;
+            SignalGeneratorType sgType = SignalGeneratorType.SawTooth;
+
+            ISampleProvider shortRest = new SignalGenerator()
+            {
+                Gain = 0
+            }.Take(TimeSpan.FromMilliseconds(200));
+
+            ISampleProvider note1, note2, note3, note4, note5;
+
+            Solfeg solfeg = new Solfeg(frequency);
+
+            switch (drillType)
+            {
+
+                case L1C2DrillType.DoDoMiMiMi:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.DoFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.DoFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    break;
+
+                case L1C2DrillType.FaFaLaLaSo:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.FaFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.FaFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.LaFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.LaFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    break;
+
+                case L1C2DrillType.SoSoTiTiDo:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.TiFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.TiFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.HighDoFrequency, sgType, noteDuration);
+                    break;
+
+                case L1C2DrillType.TiTiSoSoSo:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.TiFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.TiFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.SoFrequency, sgType, noteDuration);
+                    break;
+
+                case L1C2DrillType.LaLaFaFaMi:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.LaFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.LaFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.FaFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.FaFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    break;
+
+                case L1C2DrillType.MiMiDoDoDo:
+                    note1 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(gain, solfeg.MiFrequency, sgType, noteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(gain, solfeg.DoFrequency, sgType, noteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(gain, solfeg.DoFrequency, sgType, noteDuration);
+                    note5 = NAudioHelper.GetSampleProvider(gain, solfeg.DoFrequency, sgType, noteDuration);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"L1C2DrillType '{drillType}' is not supported.");
+            }
+
+            var phrase = note1
+                .FollowedBy(shortRest)
+                .FollowedBy(note2)
+                .FollowedBy(shortRest)
+                .FollowedBy(note3)
+                .FollowedBy(shortRest)
+                .FollowedBy(note4)
+                .FollowedBy(shortRest)
+                .FollowedBy(note5);
+
+            var stwp = new SampleToWaveProvider(phrase);
+
+            MemoryStream wavStream = new MemoryStream();
+            WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
+            wavStream.Position = 0;
+            return wavStream;
+        }
+    }
+}
---
+++ EarTrainingLibrary/Enums/L1C2DrillType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarTrainingLibrary.Enums
+{
+    public enum L1C2DrillType
+    {
+        DoDoMiMiMi = 0,
+        FaFaLaLaSo = 1,
+        SoSoTiTiDo = 2,
+        TiTiSoSoSo = 3,
+        LaLaFaFaMi = 4,
+        MiMiDoDoDo = 5
+    }
+}
