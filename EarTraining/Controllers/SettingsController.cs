9426f22b123e12afe263883f74421025ea0c8c6c
Mike Hildner <mike.hildner@gmail.com>
2019-05-18 22:23:03 -0400

    Begin settings page.

---
+++ EarTraining/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EarTraining.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            foreach (var sgt in Enum.GetValues(typeof(SignalGeneratorType)))
+            {
+                var s = sgt.ToString();
+                var i = (int)sgt;
+            }
+
+            return View();
+        }
+    }
+}
