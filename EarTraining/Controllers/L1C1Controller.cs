5625a631966c745dd96ec4afaa91f1cde30f8193
Mike Hildner <mike.hildner@gmail.com>
2019-05-23 22:10:09 -0400

    L1C1 piano samples.

--- EarTraining/Controllers/L1C1Controller.cs
+++ EarTraining/Controllers/L1C1Controller.cs
@@ -1,13 +1,11 @@
 ﻿using EarTrainingLibrary.Enums;
 using EarTrainingLibrary.NAudio;
 using EarTrainingLibrary.Utility;
-using NAudio.Lame;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using System;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Web.Mvc;
 using WaveLibrary;
 
@@ -27,13 +25,25 @@ namespace EarTraining.Controllers
         public ActionResult GetResolution(double frequency, int type)
         {
             ResolutionType rt = (ResolutionType)type;
-            var retMs = new MemoryStream();
+            MemoryStream retMs = new MemoryStream();
             MemoryStream wavStream = GetResolution(frequency, rt);
 
             wavStream.WavToMp3File(out string fileName);
             return Redirect($"~/Temp/{fileName}");
 
         }
+
+        public ActionResult GetResolutionEx(string doNoteName, int type)
+        {
+            ResolutionType rt = (ResolutionType)type;
+            MemoryStream retMs = new MemoryStream();
+            MemoryStream wavStream = GetResolution(doNoteName, rt);
+
+            wavStream.WavToMp3File(out string fileName);
+            return Redirect($"~/Temp/{fileName}");
+
+        }
+
 
         private MemoryStream GetResolution(double frequency, ResolutionType resolutionType)
         {
@@ -97,7 +107,7 @@ namespace EarTraining.Controllers
                     throw new NotSupportedException($"ResolutionType '{resolutionType}' is not supported.");
             }
 
-            var phrase = note1
+            ISampleProvider phrase = note1
                 .FollowedBy(shortRest)
                 .FollowedBy(note2)
                 .FollowedBy(shortRest)
@@ -105,7 +115,87 @@ namespace EarTraining.Controllers
                 .FollowedBy(shortRest)
                 .FollowedBy(note4);
 
-            var stwp = new SampleToWaveProvider(phrase);
+            SampleToWaveProvider stwp = new SampleToWaveProvider(phrase);
+
+            MemoryStream wavStream = new MemoryStream();
+            WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
+            wavStream.Position = 0;
+            return wavStream;
+        }
+
+        private MemoryStream GetResolution(string doNoteName, ResolutionType resolutionType)
+        {
+            double bpm = 60;
+            double quarterNotemillis = (60 / bpm) * 1000;
+            TimeSpan quarterNoteDuration = TimeSpan.FromMilliseconds(quarterNotemillis);
+            TimeSpan halfNoteDuration = quarterNoteDuration.Add(quarterNoteDuration);
+            TimeSpan wholeNoteDuration = halfNoteDuration.Add(halfNoteDuration);
+
+            string doFileName = NAudioHelper.GetFileNameFromNoteName(doNoteName);
+            doFileName = Path.GetFileName(doFileName);
+            int doNoteNumber = int.Parse(doFileName.Split('.')[0]);
+
+            ISampleProvider shortRest = new SignalGenerator()
+            {
+                Gain = 0
+            }.Take(TimeSpan.FromMilliseconds(200));
+
+            ISampleProvider note1;
+            ISampleProvider note2;
+            ISampleProvider note3;
+            ISampleProvider note4;
+
+            switch (resolutionType)
+            {
+
+                case ResolutionType.DoDoReDo:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.Up2nd, halfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber, wholeNoteDuration);
+                    break;
+
+                case ResolutionType.DoDoFaMi:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpPerfect4th, halfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor3rd , wholeNoteDuration);
+                    break;
+
+                case ResolutionType.DoDoLaSo:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor6th, halfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpPerfect5th, wholeNoteDuration);
+                    break;
+
+                case ResolutionType.DoDoTiHighDo:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor7th, halfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber + 12, wholeNoteDuration);
+                    break;
+
+                case ResolutionType.DoDoLowTiDo:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber, quarterNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.DownHalfStep, halfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber, wholeNoteDuration);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"ResolutionType '{resolutionType}' is not supported.");
+            }
+
+            ISampleProvider phrase = note1
+                .FollowedBy(shortRest)
+                .FollowedBy(note2)
+                .FollowedBy(shortRest)
+                .FollowedBy(note3)
+                .FollowedBy(shortRest)
+                .FollowedBy(note4);
+
+            SampleToWaveProvider stwp = new SampleToWaveProvider(phrase);
 
             MemoryStream wavStream = new MemoryStream();
             WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
--- EarTrainingLibrary/Enums/Interval.cs
+++ EarTrainingLibrary/Enums/Interval.cs
@@ -8,9 +8,12 @@ namespace EarTrainingLibrary.Enums
 {
     public static class Interval
     {
+        public static int DownHalfStep { get { return -1; } }
+        public static int Up2nd { get { return 2; } }
         public static int UpMajor3rd{get{ return 4; }}
         public static int UpPerfect4th { get { return 5; } }
         public static int UpPerfect5th { get { return 7; } }
-
+        public static int UpMajor6th { get { return 9; } }
+        public static int UpMajor7th { get { return 11; } }
     }
 }
