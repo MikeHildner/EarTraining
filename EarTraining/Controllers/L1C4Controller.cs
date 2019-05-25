831d50079375381269e4b06bac5f03e58a56585e
Mike Hildner <mike.hildner@gmail.com>
2019-05-25 19:57:05 -0400

    First 3 chord progression complete.

--- EarTraining/Controllers/L1C4Controller.cs
+++ EarTraining/Controllers/L1C4Controller.cs
@@ -24,7 +24,7 @@ namespace EarTraining.Controllers
             return View();
         }
 
-        public ActionResult GetProgressionEx(string doNoteName, int progressiontype)
+        public ActionResult Get2ChordProgressionEx(string doNoteName, int progressiontype)
         {
             var progressionType = (ProgressionType)progressiontype;
 
@@ -76,6 +76,64 @@ namespace EarTraining.Controllers
 
             var phrase = msp1
                 .FollowedBy(msp2);
+
+            var stwp = new SampleToWaveProvider(phrase);
+
+            MemoryStream wavStream = new MemoryStream();
+            WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
+            wavStream.Position = 0;
+
+            wavStream.WavToMp3File(out string fileName);
+            return Redirect($"~/Temp/{fileName}");
+        }
+
+        public ActionResult Get3ChordProgressionEx(string doNoteName, int progressiontype)
+        {
+            var progressionType = (ProgressionType)progressiontype;
+
+            TimeSpan noteDuration = TimeSpan.FromSeconds(1);
+
+            string doFileName = NAudioHelper.GetFileNameFromNoteName(doNoteName);
+            doFileName = Path.GetFileName(doFileName);
+            int doNoteNumber = int.Parse(doFileName.Split('.')[0]);
+
+            ISampleProvider[] samples1;
+            ISampleProvider[] samples2;
+            ISampleProvider[] samples3;
+
+            switch (progressionType)
+            {
+                case ProgressionType.Four2ndFive1stOneRoot:
+                    samples1 = Inversion.CreateTriadInversionEx(InversionType.LowSecondInversion, noteDuration, doNoteNumber + Interval.UpPerfect4th, doNoteNumber + Interval.UpMajor6th, doNoteNumber + Interval.Up1Octave);
+                    samples2 = Inversion.CreateTriadInversionEx(InversionType.LowFirstInversion, noteDuration, doNoteNumber + Interval.UpPerfect5th, doNoteNumber + Interval.UpMajor7th, doNoteNumber + Interval.UpMajor9th);
+                    samples3 = Inversion.CreateTriadInversionEx(InversionType.RootPosition, noteDuration.Add(noteDuration), doNoteNumber, doNoteNumber + Interval.UpMajor3rd, doNoteNumber + Interval.UpPerfect5th);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"ProgressionType {progressionType} is not supported.");
+            }
+
+            // First chord.
+            MixingSampleProvider msp1 = new MixingSampleProvider(samples1[0].WaveFormat);
+            msp1.AddMixerInput(samples1[0]);
+            msp1.AddMixerInput(samples1[1]);
+            msp1.AddMixerInput(samples1[2]);
+
+            // Second chord.
+            MixingSampleProvider msp2 = new MixingSampleProvider(samples2[0].WaveFormat);
+            msp2.AddMixerInput(samples2[0]);
+            msp2.AddMixerInput(samples2[1]);
+            msp2.AddMixerInput(samples2[2]);
+
+            // Third chord.
+            MixingSampleProvider msp3 = new MixingSampleProvider(samples3[0].WaveFormat);
+            msp3.AddMixerInput(samples3[0]);
+            msp3.AddMixerInput(samples3[1]);
+            msp3.AddMixerInput(samples3[2]);
+
+            var phrase = msp1
+                .FollowedBy(msp2)
+                .FollowedBy(msp3);
 
             var stwp = new SampleToWaveProvider(phrase);
 
--- EarTrainingLibrary/Enums/ProgressionType.cs
+++ EarTrainingLibrary/Enums/ProgressionType.cs
@@ -2,9 +2,13 @@
 {
     public enum ProgressionType
     {
+        // Two chord progressions.
         Four2ndToOneRoot = 0,
         Five1stToOneRoot = 1,
         FiveRootToFourRoot = 2,
-        OneRootToFour2nd = 3
+        OneRootToFour2nd = 3,
+
+        // Three chord progressions.
+        Four2ndFive1stOneRoot = 4
     }
 }
