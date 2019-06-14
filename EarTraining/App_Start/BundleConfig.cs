b3ed9c5acdd3df4fe8b967eaa3ac6d1cbc54a3bb
Mike Hildner <mike.hildner@gmail.com>
2019-06-14 18:02:16 -0400

    Add floating play the DO button. Add fontawesome.

--- EarTraining/App_Start/BundleConfig.cs
+++ EarTraining/App_Start/BundleConfig.cs
@@ -28,7 +28,8 @@ namespace EarTraining
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/css-toggle-switch.css"));
+                      "~/Content/css-toggle-switch.css",
+                      "~/Content/font-awesome.css"));
         }
     }
 }
