838194cbb6b818a6b765219e03a8cb30a211610d
Mike Hildner <mike.hildner@gmail.com>
2019-06-16 10:27:48 -0400

    Begin level 1 chapter 5.

---
+++ EarTraining/Controllers/L1C5Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WaveLibrary;
+
+namespace EarTraining.Controllers
+{
+    public class L1C5Controller : Controller
+    {
+        public L1C5Controller()
+        {
+            Pitch pitch = new Pitches().Random();
+            ViewBag.Pitch = pitch;
+        }
+
+        public ActionResult MelodicIntervals()
+        {
+            return View();
+        }
+    }
+}
