df1b5a5dc8dd26ae7cfe4bcdc756701c954066d7
Mike Hildner <mike.hildner@gmail.com>
2019-09-22 08:53:05 -0400

    Begin L1C6 three chord progressions.

--- EarTraining/Controllers/L1C6Controller.cs
+++ EarTraining/Controllers/L1C6Controller.cs
@@ -443,5 +443,62 @@ namespace EarTraining.Controllers
             wavStream.WavToMp3File(out string fileName);
             return Redirect($"~/Temp/{fileName}");
         }
+
+        public ActionResult Get3ChordProgressionEx(string doNoteName, int progressiontype)
+        {
+            var progressionType = (ProgressionType2)progressiontype;
+
+            TimeSpan noteDuration1 = TimeSpan.FromSeconds(2);
+            TimeSpan noteDuration2 = noteDuration1;
+            TimeSpan noteDuration3 = noteDuration1;
+
+            string doFileName = NAudioHelper.GetFileNameFromNoteName(doNoteName);
+            doFileName = Path.GetFileName(doFileName);
+            int doNoteNumber = int.Parse(doFileName.Split('.')[0]);
+
+            ISampleProvider[] samples1;
+            ISampleProvider[] samples2;
+            ISampleProvider[] samples3;
+
+            switch (progressionType)
+            {
+                case ProgressionType2.OneRootFive1stSixMin1st:
+                    samples1 = Inversion.CreateTriadInversionEx(InversionType.RootPosition, noteDuration1, doNoteNumber, doNoteNumber + Interval.UpMajor3rd, doNoteNumber + Interval.UpPerfect5th);
+                    samples2 = Inversion.CreateTriadInversionEx(InversionType.LowFirstInversion, noteDuration2, doNoteNumber + Interval.UpPerfect5th, doNoteNumber + Interval.UpMajor7th, doNoteNumber + Interval.UpMajor9th);
+                    samples3 = Inversion.CreateTriadInversionEx(InversionType.LowFirstInversion, noteDuration3, doNoteNumber + Interval.UpMajor6th, doNoteNumber + Interval.UpPerfectOctave, doNoteNumber + Interval.UpMajor10th);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"ProgressionType {progressionType} is not supported.");
+            }
+
+            MixingSampleProvider msp1 = new MixingSampleProvider(samples1[0].WaveFormat);
+            msp1.AddMixerInput(samples1[0]);
+            msp1.AddMixerInput(samples1[1]);
+            msp1.AddMixerInput(samples1[2]);
+
+            MixingSampleProvider msp2 = new MixingSampleProvider(samples2[0].WaveFormat);
+            msp2.AddMixerInput(samples2[0]);
+            msp2.AddMixerInput(samples2[1]);
+            msp2.AddMixerInput(samples2[2]);
+
+            MixingSampleProvider msp3 = new MixingSampleProvider(samples3[0].WaveFormat);
+            msp3.AddMixerInput(samples3[0]);
+            msp3.AddMixerInput(samples3[1]);
+            msp3.AddMixerInput(samples3[2]);
+
+            var phrase = msp1
+                .FollowedBy(msp2)
+                .FollowedBy(msp3);
+
+            var stwp = new SampleToWaveProvider(phrase);
+
+            MemoryStream wavStream = new MemoryStream();
+            WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
+            wavStream.Position = 0;
+
+            wavStream.WavToMp3File(out string fileName);
+            return Redirect($"~/Temp/{fileName}");
+        }
     }
 }
--- EarTrainingLibrary/Enums/ProgressionType2.cs
+++ EarTrainingLibrary/Enums/ProgressionType2.cs
@@ -9,7 +9,7 @@ namespace EarTrainingLibrary.Enums
     // ProgressionType enum was getting a little too large to easily look at.
     public enum ProgressionType2
     {
-        // I IV V vi ii 2 chord progressions.
+        // I IV V vi iii 2 chord progressions.
         OneRootToSixMin1st = 0,
         OneRootToFour2nd = 1,
         FourRootToFiveRoot = 2,
@@ -20,6 +20,20 @@ namespace EarTrainingLibrary.Enums
         One2ndToSixMinRoot = 7,
         ThreeMinRootToFive2nd = 8,
         Five2ndToSixMin2nd = 9,
-        Four2ndToThreeMin2nd = 10
+        Four2ndToThreeMin2nd = 10,
+
+        // I IV V vi iii 3 chord progressions.
+        OneRootFive1stSixMin1st = 11,
+        One1stThreeMinRootFourRoot = 12,
+        SixMin2ndFive2ndFour2nd = 13,
+        ThreeMin2ndOneRootSixMin1st = 14,
+        FourRootOne1stThreeMinRoot = 15,
+        SixMin2ndThreeMinRootOne1st = 16,
+        One2ndFour1stFiveRoot = 17,
+        SixMinRootOne2ndFour1st = 18,
+        ThreeMin1stSixMinRootFour1st = 19,
+        OneRootSixMin1stFive1st = 20,
+        Five2ndOne1stSixMinSixMin2nd = 21,
+        FourRootFive2ndSixMin2nd = 22
     }
 }
