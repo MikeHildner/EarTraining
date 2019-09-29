01f259410ea60c88432d3bbc0fcdd00668902d11
Mike Hildner <mike.hildner@gmail.com>
2019-09-29 19:29:54 -0400

    Begin L1C7 vocal drills.

--- EarTraining/Controllers/L1C7Controller.cs
+++ EarTraining/Controllers/L1C7Controller.cs
@@ -5,6 +5,7 @@ using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -27,6 +28,62 @@ namespace EarTraining.Controllers
             return View();
         }
 
+        public ActionResult VocalDrills()
+        {
+            return View();
+        }
+
+        public ActionResult GetMelodicDrill(string doNoteName, int type)
+        {
+            L1C7MelodicDrillType drillType = (L1C7MelodicDrillType)type;
+            MemoryStream wavStream = GetMelodicDrill(doNoteName, drillType);
+
+            wavStream.WavToMp3File(out string fileName);
+            return Redirect($"~/Temp/{fileName}");
+        }
+
+        private MemoryStream GetMelodicDrill(string doNoteName, L1C7MelodicDrillType drillType)
+        {
+            double bpm = double.Parse(ConfigurationManager.AppSettings["MelodicDrillBPM"]);
+            double quarterNoteMillis = (60 / bpm) * 1000;
+
+            TimeSpan eigthNoteDuration = TimeSpan.FromMilliseconds(quarterNoteMillis / 2);
+            TimeSpan quarterNoteDuration = TimeSpan.FromMilliseconds(quarterNoteMillis);
+            TimeSpan halfNoteDuration = TimeSpan.FromMilliseconds(quarterNoteMillis * 2);
+            TimeSpan doubleDottedHalfNoteDuration = halfNoteDuration.Add(eigthNoteDuration);
+            TimeSpan wholeNoteDuration = TimeSpan.FromMilliseconds(quarterNoteMillis * 4);
+
+            string doFileName = NAudioHelper.GetFileNameFromNoteName(doNoteName);
+            doFileName = Path.GetFileName(doFileName);
+            int doNoteNumber = int.Parse(doFileName.Split('.')[0]);
+
+            ISampleProvider note1, note2, note3, note4;
+
+            switch (drillType)
+            {
+                case L1C7MelodicDrillType.MiMiFaMi:
+                    note1 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor3rd, quarterNoteDuration);
+                    note2 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor3rd, eigthNoteDuration);
+                    note3 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpPerfect4th, doubleDottedHalfNoteDuration);
+                    note4 = NAudioHelper.GetSampleProvider(doNoteNumber + Interval.UpMajor3rd, wholeNoteDuration);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"L1C7MelodicDrillType '{drillType}' is not supported.");
+            }
+
+            var phrase = note1
+                .FollowedBy(note2)
+                .FollowedBy(note3)
+                .FollowedBy(note4);
+
+            var stwp = new SampleToWaveProvider(phrase);
+
+            MemoryStream wavStream = new MemoryStream();
+            WaveFileWriter.WriteWavFileToStream(wavStream, stwp);
+            wavStream.Position = 0;
+            return wavStream;
+        }
         public ActionResult Get2ChordProgressionEx(string doNoteName, int progressiontype)
         {
             var progressionType = (ProgressionType3)progressiontype;
---
+++ EarTrainingLibrary/Enums/L1C7MelodicDrillType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarTrainingLibrary.Enums
+{
+    public enum L1C7MelodicDrillType
+    {
+        MiMiFaMi = 0,
+    }
+}
