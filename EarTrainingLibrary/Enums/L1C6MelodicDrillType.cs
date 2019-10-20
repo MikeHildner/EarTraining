6e438d197933f83858e252cf28abfc7e5c13ba55
Mike Hildner <mike.hildner@gmail.com>
2019-10-20 10:57:31 -0400

    Begin L1C6 melodic interval identification.

--- EarTrainingLibrary/Enums/L1C6MelodicDrillType.cs
+++ EarTrainingLibrary/Enums/L1C6MelodicDrillType.cs
@@ -8,6 +8,8 @@ namespace EarTrainingLibrary.Enums
 {
     public enum L1C6MelodicDrillType
     {
+        // Vocal drills.
+
         // Major 2nds.
         DoDoReReDo2nd = 0,
         ReReMiMiMi2nd = 1,
@@ -30,6 +32,32 @@ namespace EarTrainingLibrary.Enums
         ReReMiMiMi7th = 16,
         FaFaSoSoSo7th = 17,
         SoSoLaLaSo7th = 18,
-        LaLaTiTiDo7th = 19
+        LaLaTiTiDo7th = 19,
+
+        // Melodic intervals.
+
+        // Major 2nds.
+        DoRe2Asc = 20,
+        ReMi2Asc = 21,
+        FaSo2Asc = 22,
+        SoLa2Asc = 23,
+        LaTi2Asc = 24,
+        ReDo2Desc = 25,
+        MiRe2Desc = 26,
+        SoFa2Desc = 27,
+        LaSo2Desc = 28,
+        TiLa2Desc = 29,
+
+        // Minor 7ths.
+        ReDo7Asc = 30,
+        MiRe7Asc = 31,
+        SoFa7Asc = 32,
+        LaSo7Asc = 33,
+        TiLa7Asc = 34,
+        DoRe7Desc = 35,
+        ReMi7Desc = 36,
+        FaSo7Desc = 37,
+        SoLa7Desc = 38,
+        LaTi7Desc = 39
     }
 }
