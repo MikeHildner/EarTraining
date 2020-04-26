49ba20597516273a64bdc29ef6640488120a12e0
Mike Hildner <Mike.Hildner@sunhydraulics.com>
2020-04-26 13:57:20 -0400

    Add theme page.

---
+++ EarTraining/Controllers/ThemeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EarTraining.Controllers
+{
+    public class ThemeController : BaseController
+    {
+        // GET: Theme
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
