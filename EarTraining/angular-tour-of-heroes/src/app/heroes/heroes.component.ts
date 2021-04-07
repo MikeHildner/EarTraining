ba0d9978a34d0e1323f060e12640b0e9dcf4bfda
Mike Hildner <Mike.Hildner@sunhydraulics.com>
2021-04-07 10:09:49 -0400

    Tutorial chapter 4 completed.

--- EarTraining/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ EarTraining/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,14 +14,26 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}, name=${hero.name}`);
   }
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() { }
+  //getHeroes(): void {
+  //  this.heroes = this.heroService.getHeroes();
+  //}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
